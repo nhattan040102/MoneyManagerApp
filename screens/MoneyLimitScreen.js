d68e16234fbb9f6b151dcff53a4af3001bbc11fb
nhattan040102-acc <tantran.040102@gmail.com>
2022-06-15 20:52:33 +0700

    fix conflict

--- screens/MoneyLimitScreen.js
+++ screens/MoneyLimitScreen.js
@@ -1,30 +1,16 @@
-<<<<<<< HEAD
-import { React } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import ExpenseLimit from "../components/ExpenseLimit";
-=======
 import { React } from 'react';
 import { View, Text, StyleSheet, StatusBar, Platform } from 'react-native';
-import NoBudgetLimitCard from '../components/NoBudgetLimitCard';
->>>>>>> 1c7056ee1143baf2ac30264cabd640fa21121176
+import ExpenseLimit from '../components/ExpenseLimit';
 
 const MoneyLimitScreen = (props) => {
-  return (
-    <View style={styles.screen}>
-      <ExpenseLimit />
-    </View>
-  );
+    return (
+        <View style={styles.screen}>
+            <ExpenseLimit />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-<<<<<<< HEAD
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
-=======
     screen: {
         flex: 1,
         justifyContent: 'center',
@@ -32,6 +18,5 @@ const styles = StyleSheet.create({
         marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     }
 })
->>>>>>> 1c7056ee1143baf2ac30264cabd640fa21121176
 
 export default MoneyLimitScreen;
