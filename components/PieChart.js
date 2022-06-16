e40bda2e6096684a409cc6c5dc237c7dd7140b49
nhattan040102-acc <tantran.040102@gmail.com>
2022-06-16 14:13:54 +0700

    add pie chart component

---
+++ components/PieChart.js
@@ -0,0 +1,70 @@
+// src/components/FinancePieChart.js
+
+import React from 'react';
+import { View, Text, Dimensions, StyleSheet } from 'react-native';
+
+import { PieChart } from 'react-native-chart-kit';
+import { FONTSIZE } from '../constants/constants';
+
+// import Legend from './Legend';
+
+const screenWidth = Dimensions.get('window').width;
+
+const chartConfig = {
+    backgroundGradientFrom: '#1E2923',
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: '#08130D',
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2,
+    useShadowColorFromDataset: false,
+};
+
+
+function FinancePieChart({ title, data }) {
+    return (
+        <View style={styles.container}>
+            <View style={styles.titleContainer}>
+                <Text style={{ fontSize: FONTSIZE.header2, color: 'rgb(45,139, 126)' }}>{title}</Text>
+            </View>
+
+            <View style={styles.bodyContainer}>
+                <View style={styles.chartContainer}>
+                    <PieChart
+                        data={data}
+                        width={screenWidth}
+                        height={200}
+                        chartConfig={chartConfig}
+                        accessor={"population"}
+                        backgroundColor={'transparent'}
+                        center={[0, 0]}
+                        hasLegend={true}
+                    />
+                </View>
+
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 10,
+    },
+    titleContainer: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    bodyContainer: {
+        flexDirection: 'row',
+    },
+    chartContainer: {
+        flex: 1,
+    },
+    legendContainer: {
+        flex: 1,
+        marginTop: 20,
+    },
+});
+
+export default FinancePieChart;
--- screens/ReportScreen.js
+++ screens/ReportScreen.js
@@ -1,8 +1,47 @@
 import { React, useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, Platform, StatusBar } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, Platform, StatusBar, ScrollView } from 'react-native';
 import { FONTSIZE } from '../constants/constants';
 import { Entypo } from '@expo/vector-icons';
 import { Foundation } from '@expo/vector-icons';
+import FinancePieChart from '../components/PieChart';
+
+const data = [
+    {
+        name: "Seoul",
+        population: 0,
+        color: "rgba(131, 167, 234, 1)",
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+    },
+    {
+        name: "Toronto",
+        population: 2800000,
+        color: "#F00",
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+    },
+    {
+        name: "Beijing",
+        population: 527612,
+        color: "red",
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+    },
+    {
+        name: "New York",
+        population: 8538000,
+        color: "#ffffff",
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+    },
+    {
+        name: "Moscow",
+        population: 11920000,
+        color: "rgb(0, 0, 255)",
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+    }
+];
 
 const ReportScreen = props => {
     const [currentState, setCurrentState] = useState('COLUMN CHART');
@@ -12,12 +51,12 @@ const ReportScreen = props => {
         <View style={styles.screen}>
 
             {/* view for displaying header bar: COLUMN CHAT and PIE CHART */}
-            {/* <SafeAreaView style={styles.headerBar}>
+            <SafeAreaView style={styles.headerBar}>
                 <TouchableOpacity
                     style={styles.category}
                     onPress={() => setCurrentState('COLUMN CHART')}
                 >
-                    <Entypo name="bar-graph" size={25} color="white" />
+                    <Entypo name="bar-graph" size={FONTSIZE.header2} color="white" />
                     <Text style={[styles.cate_text]}>BIỂU ĐỒ CỘT</Text>
                     <View style={{ width: '98%', borderWidth: currentState == "COLUMN CHART" ? 2 : 0, borderColor: '#00C897', position: 'absolute', bottom: -3 }}></View>
                 </TouchableOpacity>
@@ -26,11 +65,19 @@ const ReportScreen = props => {
                     style={styles.category}
                     onPress={() => setCurrentState('PIE CHART')}
                 >
-                    <Foundation name="graph-pie" size={25} color="white" />
+                    <Foundation name="graph-pie" size={FONTSIZE.header2} color="white" />
                     <Text style={[styles.cate_text]}>BIỂU ĐỒ TRÒN</Text>
                     <View style={{ width: '98%', borderWidth: currentState == "PIE CHART" ? 2 : 0, borderColor: '#00C897', position: 'absolute', bottom: -3 }}></View>
                 </TouchableOpacity>
-            </SafeAreaView> */}
+            </SafeAreaView>
+            {
+                currentState == "PIE CHART" ? <ScrollView contentContainerStyle={{ paddingBottom: 100 }}>
+                    <FinancePieChart title={"Loại giao dịch"} data={data} />
+                    <FinancePieChart title={"Chi tiêu"} data={data} />
+                    <FinancePieChart title={"Thu nhập"} data={data} />
+                </ScrollView> : <View></View>
+            }
+
 
 
         </View>
@@ -41,6 +88,7 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         height: '100%',
+
     },
 
     headerBar: {
@@ -57,7 +105,7 @@ const styles = StyleSheet.create({
     },
 
     cate_text: {
-        fontSize: FONTSIZE.header1,
+        fontSize: FONTSIZE.header2,
         fontWeight: '500',
         color: 'white',
         padding: 5,
